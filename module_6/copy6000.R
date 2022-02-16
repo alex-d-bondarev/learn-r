e7e6ac225bbbe7a3fe9b67d04e5a1647e7ed9f4d
Sasha Bondarev <sasha.bondarev@vonage.com>
2022-02-16 20:24:30 +0000

    replace bash with R for top 6000 rows file

---
+++ module_6/copy6000.R
@@ -0,0 +1,5 @@
+rawData <- read.csv(file="./data/raw/world_salaries.csv")
+class(rawData)
+nrow(rawData)
+just6000 <- head(rawData, n=6000)
+rawData <- write.csv(just6000, file="./data/6000/world_salaries_6000.csv")
